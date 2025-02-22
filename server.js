d4ed087cc7d46bce3e5c0d1d09276534f0c6d1af
NathulaDayarathne <nathuladayarathne@gmail.com>
2025-02-22 11:49:57 +0530

    add articles route and update index view to display articles

--- server.js
+++ server.js
@@ -5,9 +5,35 @@ app.set('view engine', 'ejs');
 
 
 app.get("/", (req, res) => {
-    res.render('index');
+    // res.render('index');
+
+    const articles = [
+        {
+            title: "Article 1",
+            createdAt: new Date(),
+            description: "This is article 1"
+        },
+        {
+            title: "Article 2",
+            createdAt: new Date(),
+            description: "This is article 2"
+        },
+        {
+            title: "Article 3",
+            createdAt: new Date(),
+            description: "This is article 3"
+        }
+    ];
+
+    res.render('index', { articles: articles });
 });
 
+const articlesRouter = require("./routes/articles")    
+app.use("/articles", articlesRouter)        
+
+
+
 app.listen(5000);
+
 
 
